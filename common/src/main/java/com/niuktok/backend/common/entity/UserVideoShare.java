0cbc334ba9204a8d6bde083e98e853d491be8746
peilei <714838284@qq.com>
2023-11-06 13:17:33 +0000

    feat: 增加用户视频分享表

---
+++ common/src/main/java/com/niuktok/backend/common/entity/UserVideoShare.java
@@ -0,0 +1,40 @@
+package com.niuktok.backend.common.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Table(name = "user_video_share")
+public class UserVideoShare implements Serializable {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "video_id")
+    private Long videoId;
+
+    private Integer count;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    @Column(name = "deleted_time")
+    private Date deletedTime;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ common/src/main/java/com/niuktok/backend/common/mapper/UserVideoShareMapper.java
@@ -0,0 +1,7 @@
+package com.niuktok.backend.common.mapper;
+
+import com.niuktok.backend.common.entity.UserVideoShare;
+import com.niuktok.backend.common.mapper.base.MyMapper;
+
+public interface UserVideoShareMapper extends MyMapper<UserVideoShare> {
+}
