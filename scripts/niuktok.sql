3719fcd90545907cf3ea163d0c49a28d5e100f62
peilei <714838284@qq.com>
2023-11-07 14:28:37 +0000

    chore: 同步 nacos 配置和数据库结构

--- scripts/niuktok.sql
+++ scripts/niuktok.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 01/11/2023 13:29:25
+ Date: 07/11/2023 22:26:36
 */
 
 SET NAMES utf8mb4;
@@ -32,6 +32,7 @@ CREATE TABLE `comment` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -49,7 +50,8 @@ CREATE TABLE `message` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
@@ -67,7 +69,8 @@ CREATE TABLE `user` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `unique_username` (`username`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -80,14 +83,15 @@ CREATE TABLE `user_auth` (
   `user_id` bigint unsigned NOT NULL,
   `identity_type` tinyint NOT NULL,
   `identifier` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `credential` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `credential` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_identifier` (`identifier`,`deleted_time`) USING BTREE,
-  UNIQUE KEY `unique_user_identifier_type` (`user_id`,`identity_type`,`deleted_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_user_identifier_type` (`user_id`,`identity_type`,`deleted_time`) USING BTREE,
+  UNIQUE KEY `unique_identifier` (`identifier`,`identity_type`,`deleted_time`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user_comment_like
@@ -100,7 +104,8 @@ CREATE TABLE `user_comment_like` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `unique_user_comment` (`user_id`,`comment_id`,`deleted_time`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -115,7 +120,8 @@ CREATE TABLE `user_follow` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `unique_user_follower` (`user_id`,`follower_id`,`deleted_time`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -130,9 +136,11 @@ CREATE TABLE `user_video_favorite` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE,
+  KEY `video_index` (`video_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user_video_like
@@ -145,9 +153,29 @@ CREATE TABLE `user_video_like` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE,
+  KEY `video_index` (`video_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for user_video_share
+-- ----------------------------
+DROP TABLE IF EXISTS `user_video_share`;
+CREATE TABLE `user_video_share` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
+  `video_id` bigint unsigned NOT NULL,
+  `count` int unsigned DEFAULT '0',
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_time` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE,
+  KEY `video_index` (`video_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user_video_view
@@ -162,9 +190,11 @@ CREATE TABLE `user_video_view` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_user_video` (`user_id`,`video_id`,`deleted_time`) USING BTREE,
+  KEY `video_index` (`video_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for video
@@ -173,19 +203,39 @@ DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video` (
   `id` bigint unsigned NOT NULL,
   `user_id` bigint unsigned NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `description` varchar(1024) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `video_key` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
   `video_path` varchar(1024) COLLATE utf8mb4_general_ci NOT NULL,
   `cover_path` varchar(1024) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'https://upload.shejihz.com/2019/03/43dd7e490b0baac64d22e3d453483886.jpg',
-  `play_num` bigint unsigned NOT NULL DEFAULT '0',
+  `view_num` bigint unsigned NOT NULL DEFAULT '0',
   `like_num` bigint unsigned NOT NULL DEFAULT '0',
   `favorite_num` bigint unsigned NOT NULL DEFAULT '0',
   `share_num` bigint unsigned NOT NULL DEFAULT '0',
   `info` json NOT NULL,
+  `partition_id` bigint unsigned NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_user_video_key` (`user_id`,`video_key`,`deleted_time`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for video_partition
+-- ----------------------------
+DROP TABLE IF EXISTS `video_partition`;
+CREATE TABLE `video_partition` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_time` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_name` (`name`,`deleted_time`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for video_tag
@@ -197,8 +247,10 @@ CREATE TABLE `video_tag` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `unique_title` (`title`,`deleted_time`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for video_tag_link
@@ -211,8 +263,9 @@ CREATE TABLE `video_tag_link` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `unique_tag_video` (`tag_id`,`video_id`,`deleted_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=556 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
